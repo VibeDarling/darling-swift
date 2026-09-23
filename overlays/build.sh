8ed4e748a26afdd671204a1269a97d0c3c6287df
Cristian Magherusan-Stanciu <cristi@leanercloud.com>
2026-09-23 12:38:33 +0200

    build: allow focused Foundation overlay builds

--- overlays/build.sh
+++ overlays/build.sh
@@ -141,6 +141,13 @@ printf '_$s11_RopeModule*\n_$s28InternalCollectionsUtilities*\n' > "$out/unexpor
 foundation="$DARLING_ROOT/System/Library/Frameworks/Foundation.framework/Versions/C/Foundation"
 build_module Foundation "$here"/Foundation/*.swift -- -package-name swift-foundation --link -unexported_symbols_list "$out/unexported.txt" "$out/obj/_RopeModule.o" "$out/obj/InternalCollectionsUtilities.o" "$foundation" "$corefoundation" -lswiftDarwin -lswiftObjectiveC -lswiftCoreFoundation -lswiftDispatch
 
+# Build just Foundation for focused overlay changes without rebuilding or replacing
+# the other shipped Swift dylibs.
+if [ "${DARLING_SWIFT_FOUNDATION_ONLY:-0}" = 1 ]; then
+	echo "built Foundation overlay at $out/libswiftFoundation.dylib"
+	exit 0
+fi
+
 coregraphics="$DARLING_ROOT/System/Library/Frameworks/CoreGraphics.framework/Versions/A/CoreGraphics"
 build_module CoreGraphics "$here/CoreGraphics/CoreGraphics.swift" -- -Xcc -fmodule-map-file="$here/CoreGraphics/shims/module.modulemap" --link "$coregraphics" "$corefoundation" -lswiftCoreFoundation -lswiftDarwin
 
